59b2800f4a301da7728489375f60b31ed3a23abc
Walid Algazali <walidalgazali14@gmail.com>
2021-09-23 14:26:17 +0700

    Update Register
    - Untuk melakukan register akun user sudah bisa
    
    - Masih belum ada bikin message daftar akun

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -2,24 +2,35 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Register extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+ 
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_login');
+		$this->load->library('form_validation');
+	}
+ 
 	public function index()
 	{
 		$this->load->view('register');
+	}
+ 
+	public function proses()
+	{
+		$this->form_validation->set_rules('email', 'email', 'required');
+		$this->form_validation->set_rules('password', 'password', 'required');
+		if ($this->form_validation->run()==true)
+	   	{
+			$email = $this->input->post('email');
+			$password = $this->input->post('password');
+			$this->m_login->register($email,$password);
+			$this->session->set_flashdata('success_register','Proses Pendaftaran User Berhasil');
+			redirect('login');
+		}
+		else
+		{
+			$this->session->set_flashdata('error', validation_errors());
+			redirect('register');
+		}
 	}
 }
--- application/models/m_login.php
+++ application/models/m_login.php
@@ -1,6 +1,41 @@
 <?php 
 
-class M_login extends CI_Model{	
+class M_login extends CI_Model{
+	
+		public function __construct()
+		{
+			parent::__construct();
+		}
+	 
+		function register($email,$password)
+		{
+			$data_user = array(
+				'email'=>$email,
+				'password'=>md5($password),
+			);
+			$this->db->insert('user',$data_user);
+		}
+	
+		/*function login_user($email,$password)
+		{
+			$query = $this->db->get_where('user',array('email'=>$email));
+			if($query->num_rows() > 0)
+			{
+				$data_user = $query->row();
+				if (password_verify($password, $data_user->password)) {
+					$this->session->set_userdata('email',$email);
+					//$this->session->set_userdata('is_login',TRUE);
+					return TRUE;
+				} else {
+					return FALSE;
+				}
+			}
+			else
+			{
+				return FALSE;
+			}
+		}*/	
+
 	function cek_login($table,$where){		
 		return $this->db->get_where($table,$where);
 	}	
